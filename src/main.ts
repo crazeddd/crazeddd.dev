586fbd126cfb090705e6afa3083825d2c87a9948
Crazed <cranninja@gmail.com>
2025-04-17 01:42:16 +0000

    more ui updates

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,45 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <nav>
+<pre><code id="one">
+    &lt;section class="hero"&gt;
+    &lt;div class="column gp-3"&gt;
+        &lt;h1&gt;Full Stack Dev and &lt;span class="stroke"&gt;Web Designer&lt;/span&gt;&lt;/h1&gt;
+        &lt;div class="row gp-1"&gt;
+            &lt;button class="primary"&gt;Contact Me&lt;/button&gt;
+            &lt;button&gt;Resume&lt;/button&gt;
+        &lt;/div&gt;
+    &lt;/div&gt;
+&lt;/section&gt;
+    </code></pre>
+
+<pre>
+        <code id="two">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+  font-size: 100%;
+}
+
+body {
+  color-scheme: dark;
+  background: var(--bg);
+  color: var(--txt);
+  font-family: "Special Gothic";
+  overflow-x: hidden;
+}
+        </code>
+    </pre>
+<announcement>Site Under Construction ⚠️</announcement>
+<nav>
     <div>
         <img class="pfp" src="/73705334.jpg">
         <h5>.dev</h5>
@@ -13,10 +51,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <a>Projects</a>
     </div>
     <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+            <path
+                d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+        </svg>
     </div>
-  </nav>
-  <section class="hero">
+</nav>
+<section class="hero">
     <div class="column gp-3">
         <h1>Full Stack Dev and <span class="stroke">Web Designer</span></h1>
         <div class="row gp-1">
@@ -24,8 +66,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
             <button>Resume</button>
         </div>
     </div>
-  </section>
-  <section class="showcase">
+</section>
+<section class="showcase">
     <div>
         <big>5+</big>
         <p class="muted">Years programming</p>
@@ -38,26 +80,18 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <big>6+</big>
         <p class="muted">Years running servers</p>
     </div>
-  </section>
-  <section class="about">
+</section>
+<section class="about">
     <div class="code-box">
-    <pre>&lt;section class="hero"&gt;
-    &lt;div class="column gp-3"&gt;
-        &lt;h1&gt;Full Stack Dev and &lt;span class="stroke"&gt;Web Designer&lt;/span&gt;&lt;/h1&gt;
-        &lt;div class="row gp-1"&gt;
-            &lt;button class="primary"&gt;Contact Me&lt;/button&gt;
-            &lt;button&gt;Resume&lt;/button&gt;
-        &lt;/div&gt;
-    &lt;/div&gt;
-&lt;/section&gt;
-    </pre>
     </div>
     <div class="column gp-2">
-      <h1>About Me</h1>
-      <p>At a young age I gained an appreciation for computer systems and the more technical aspects of life, this eventually led me to coding. With over 5 years of experience working with a wide range of languages i've coded numerous projects; some for fun, others for work.</p>
+        <h1>About Me</h1>
+        <p>I like coding (not done yet)</p>
     </div>
-  </section>
-  <footer><p>Built and designed with <3 by crazeddd</p></footer>
+</section>
+<footer>
+    <p>Made with <3 by @crazeddd</p>
+</footer>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
